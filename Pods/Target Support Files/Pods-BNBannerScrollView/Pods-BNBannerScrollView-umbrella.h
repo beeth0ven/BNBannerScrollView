c716edb90e2ebc3d4475ab9cbec9d2344efe9878
beeth0ven <beeth0vendev@163.com>
2016-11-15 13:07:34 +0800

    3

---
+++ Pods/Target Support Files/Pods-BNBannerScrollView/Pods-BNBannerScrollView-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_BNBannerScrollViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BNBannerScrollViewVersionString[];
+
